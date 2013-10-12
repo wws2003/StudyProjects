c8decce9a5dec05336305b9a0fc5d26e04d9d4f8
admini <admini@ubuntu.(none)>
2013-10-12 23:48:15 +0700

    some changes

--- FilterSample/deploy.sh
+++ FilterSample/deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-export DST=/home/admini/apache-tomcat-6.0.36/webapps/sapp1/WEB-INF/classes
-rm -rf $DST/*
-cp -r bin/* $DST
+export DST=/home/admini/servers/apache-tomcat-6.0.36/webapps/sapp1
+
+rm -rf $DST/WEB-INF/classes/*
+rm $DST/WEB-INF/web.xml
+rm -rf $DST/jsp/*
+
+cp -r bin/* $DST/WEB-INF/classes
+cp web.xml $DST/WEB-INF
+cp -r jsp/* $DST/jsp
