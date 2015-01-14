02b9c8b156496d22eaf3867ac28f993105188bcb
= <wws2003@gmail.com>
2015-01-15 00:47:02 +0900

    First changes in new branch

--- MySimpleServlet/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-export DST=/home/admini/apache-tomcat-6.0.36/webapps/servlet-examples/WEB-INF/classes
-export DST_LIB=/home/admini/apache-tomcat-6.0.36/webapps/servlet-examples/WEB-INF/lib
-rm -rf $DST/*
-rm -rf $DST_LIB/*
-cp -r bin/* $DST
-cp -r lib/* $DST_LIB
